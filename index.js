a89533b0ed8cbb8cba3918576ff1d7c60b1dd3e3
Fernando Henrique Silva <fernandoagro.654@gmail.com>
2023-07-19 22:24:53 -0300

    Adicionado alguns comentarios para melhor entendimento do codigo.

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+// Interação para o botão menu.
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
+// Fim da interaçaõ do botão menu
 
+// Função dark-mode e light-mode
 const darkMode = document.querySelector('.dark-mode');
 
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', () => { // Ação de quando o botão menu for clicado, ira mostrar o sideMenu.
     sideMenu.style.display = 'block';
 });
 
-closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener('click', () => { // Ação para ocultar o sideMenu.
     sideMenu.style.display = 'none';
 });
 
 darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
-    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active'); // Ativa o dark-mode.
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');// Desativa o dark-mode.
 })
+// Fim da função dark-mode e light-mode.
 
-
+// Função para orderdenar a lista de itens.
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
@@ -30,3 +34,4 @@ Orders.forEach(order => {
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
 });
+// Fim da função.
